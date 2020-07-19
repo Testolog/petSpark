9baadb684289dd459cffede942b8134b23d96b8b
whoaim <robert.nad@daxx.com>
2020-07-19 22:06:24 +0300

    done merge and aggregate files, same memory

--- src/main/scala/com/pet/task/ChunkRead.scala
+++ src/main/scala/com/pet/task/ChunkRead.scala
@@ -1,5 +1,6 @@
 package com.pet.task
 
+import scala.collection.mutable
 import scala.io.Source
 
 /**
@@ -11,41 +12,72 @@ object ChunkRead {
 
 
   def main(args: Array[String]): Unit = {
-    val files = args.head.split(",").map(Source.fromFile)
-      .map(p => new AggregateIterator(p.getLines(), sumCompress, conditionalToCompress))
-    new LinePerFileIterator[String, AggregateIterator[String]](files).foreach(println)
+    val files = args.head.split(",")
+      .map(Source.fromFile)
+      .map(_.getLines())
+    val mergedFiles = new MergeFilesIterator[String, Iterator[String]](files, compareDate)
+    new AggregateIterator[String](mergedFiles, sum, equalDate).foreach(println)
   }
 
-  class LinePerFileIterator[B, T <: Iterator[B]](inputIterators: Array[T]) extends Iterator[B] {
-    private var tmpIter = inputIterators.iterator
+  class MergeFilesIterator[Val, Iter <: Iterator[Val]](inputIterators: Array[Iter],
+                                                       orderFunctionFunction: (Val, Val) => Int)
+    extends Iterator[Val] {
 
     override def hasNext: Boolean = {
-      val anyElement = inputIterators.exists(_.hasNext)
-      if (anyElement && !tmpIter.hasNext) {
-        tmpIter = inputIterators.filter(_.hasNext).iterator
-      }
-      anyElement
+      inputIterators.exists(_.hasNext)
     }
 
-    override def next(): B = {
-      tmpIter.next().next()
+    private val allFiles: mutable.ArrayBuffer[(Iter, Val)] = new mutable.ArrayBuffer[(Iter, Val)](inputIterators.length) {
+      inputIterators.filter(_.hasNext).foreach(g => append((g, g.next())))
+    }
+
+
+    override def next(): Val = {
+      var returnValue = null.asInstanceOf[Val]
+      var indexForUpdate = 0
+      for (current <- allFiles.zipWithIndex) {
+        val currentVal = current._1._2
+        val index = current._2
+        if (returnValue == null) {
+          returnValue = currentVal
+          indexForUpdate = index
+        } else {
+          if (orderFunctionFunction(returnValue, currentVal) > 0) {
+            returnValue = currentVal
+            indexForUpdate = index
+          }
+        }
+      }
+      val (iter, _) = allFiles(indexForUpdate)
+      if (iter.hasNext) {
+        allFiles.update(indexForUpdate, (iter, iter.next()))
+      } else {
+        allFiles.remove(indexForUpdate)
+      }
+      returnValue
     }
   }
 
-  def sumCompress(str1: String, str2: String): String = {
+  def sum(str1: String, str2: String): String = {
     val value1 = str1.split(":").last
     val value2 = str2.split(":").last
     "%s:%s".format(str1.split(":").head, value1.toInt + value2.toInt)
   }
 
-  def conditionalToCompress(str1: String, str2: String): Boolean = {
+  def equalDate(str1: String, str2: String): Boolean = {
     val key1 = str1.split(":").head
     val key2 = str2.split(":").head
     key1.equals(key2)
   }
 
+  def compareDate(str1: String, str2: String): Int = {
+    val date1 = str1.split(":").head.replace("-", "").toInt
+    val date2 = str2.split(":").head.replace("-", "").toInt
+    date1.compareTo(date2)
+  }
+
   class AggregateIterator[T](inputIterators: Iterator[T],
-                             compressFunction: (T, T) => T,
+                             aggregationFunction: (T, T) => T,
                              conditionalFunction: (T, T) => Boolean) extends Iterator[T] {
     val (real, virtual) = inputIterators.duplicate
     if (virtual.hasNext) {
@@ -66,7 +98,7 @@ object ChunkRead {
       if (vCurrent != null && conditionalFunction(current, vCurrent)) {
         var compressed: T = current
         while (conditionalFunction(compressed, vCurrent) && real.hasNext && virtual.hasNext) {
-          compressed = compressFunction(compressed, vCurrent)
+          compressed = aggregationFunction(compressed, vCurrent)
           vCurrent = virtual.next()
           real.next()
         }
@@ -74,7 +106,7 @@ object ChunkRead {
         if (!virtual.hasNext) {
           if (conditionalFunction(compressed, vCurrent)) {
             real.next()
-            compressed = compressFunction(compressed, vCurrent)
+            compressed = aggregationFunction(compressed, vCurrent)
           }
         }
         compressed
