52f65e98bdb12dfa9d8e7fd7441e8d2e26a175c5
rnad <rnad@ciklum.com>
2022-01-01 21:56:37 +0200

    persists showcase, where should be aware when is called unpersists

---
+++ src/main/scala/com/pet/spark/PersistsStorageUsing.scala
@@ -0,0 +1,63 @@
+package com.pet.spark
+
+/**
+ * com.pet.spark
+ *
+ * @author Robert Nad
+ */
+object PersistsStorageUsing extends App {
+
+  import sparkSession.implicits._
+
+  sparkSession.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+  val base = Seq(
+    (1, "name1"),
+    (2, "name2"),
+    (3, "name3"),
+  ).toDF("id", "name")
+  val semi = Seq(
+    (1, 11),
+    (2, 12),
+    (3, 13),
+  ).toDF("baseid", "semiid")
+    .persist()
+  val problems = Seq(
+    (11, "error1"),
+    (12, "error2"),
+    (13, "error3"),
+  ).toDF("id", "name")
+
+  val res = base
+    .join(semi, base("id") === semi("baseid"))
+    .join(problems, semi("semiid") === problems("id"))
+
+  //  semi.count()
+  res.count()
+  res.explain()
+  println("first check of persist")
+  //there is exists persist when do action
+  sparkSession.sharedState.cacheManager.lookupCachedData(semi).foreach(println(_))
+  sparkSession.sparkContext.getPersistentRDDs.foreach(println(_))
+  println(sparkSession.sparkContext.getPersistentRDDs.size)
+  //drop persists
+  semi
+    .unpersist()
+    .count()
+
+  println("after a part of persist")
+  //persits data not exists anymore
+  println(sparkSession.sparkContext.getPersistentRDDs.size)
+  sparkSession.sharedState.cacheManager
+  //try to re persist by do action of upper level dataframe
+  res.count()
+  res.explain()
+  println("after try to re persists from upper logical plan")
+  sparkSession.sharedState.cacheManager.lookupCachedData(semi).foreach(println(_))
+  println(sparkSession.sparkContext.getPersistentRDDs.size)
+  println("cache manager work on unique ids based on logical plan")
+  res.explain()
+  //persist again
+  semi.persist().count()
+  println(sparkSession.sparkContext.getPersistentRDDs.size)
+  res.explain()
+}
